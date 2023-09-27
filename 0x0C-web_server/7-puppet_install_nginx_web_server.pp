63788664ad2bf47a3b4acb162b75bb6f250f055d
LORDyyyyy <mlord6271@gmail.com>
2023-09-27 21:53:44 +0300

    7-puppet_install_nginx_web_server.pp

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,25 @@
+# Install Nginx web server (w/ Puppet)
+
+exec { 'apt-get-update':
+  command => '/usr/bin/apt-get update',
+}
+
+package { 'nginx':
+  ensure  => installed,
+  require => Exec['apt-get-update'],
+}
+
+file { '/var/www/html/index.html':
+  content => 'Hello World 2',
+  require => Package['nginx'],
+}
+
+exec {'redirect_me':
+  command  => 'sed -i "9i\      rewrite ^/redirect_me https://www.youtube.com/watch?v=dQw4w9WgXcQ permanent;" /etc/nginx/sites-available/default',
+  provider => 'shell'
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
