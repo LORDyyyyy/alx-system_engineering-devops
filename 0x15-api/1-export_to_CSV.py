ce4c42163ea58b2f3860b1f231060b0bef80a202
LORDyyyyy <mlord6271@gmail.com>
2023-10-23 21:08:21 +0300

    2-export_to_JSON.py

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" Gathering data from an API """
+import requests
+from sys import argv
+import csv
+
+if __name__ == "__main__":
+    id = argv[1]
+    url = 'https://jsonplaceholder.typicode.com/'
+    user = requests.get(url + f"users/{id}").json()
+    todo = requests.get(url + f"todos").json()
+
+    todo = [task for task in todo if task['userId'] == int(id)]
+    emp_name = user['name']
+    total_tasks = len(todo)
+    tasks = [(task['title'], task['completed']) for task in todo]
+
+    dataset = []
+
+    for task in tasks:
+        data = {}
+        data['ID'] = int(id)
+        data['Name'] = emp_name
+        data['Status'] = task[1]
+        data['task'] = task[0]
+
+        dataset.append(data)
+
+    with open(f'{id}.csv', 'w') as csvfile:
+        fieldnames = dataset[0].keys()
+
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        for row in dataset:
+            writer.writerow(row)
