202a065a9e6f3f501594e97a4e50f129cd5caefe
LORDyyyyy <mlord6271@gmail.com>
2023-10-23 20:12:34 +0300

    0-gather_data_from_an_API.py

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Gathering data from an API """
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    id = argv[1]
+    url = 'https://jsonplaceholder.typicode.com/'
+    user = requests.get(url + f"users/{id}").json()
+    todo = requests.get(url + f"todos").json()
+
+    todo = [task for task in todo if task['userId'] == int(id)]
+    emp_name = user['name']
+    total_tasks = len(todo)
+    tasks = [task['title'] for task in todo if task['completed'] is True]
+
+    print(
+        f'Employee {emp_name} is done with tasks({len(tasks)}/{total_tasks}):')
+    for task in tasks:
+        print(f'\t{task}')
