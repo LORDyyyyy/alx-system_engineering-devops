01e275eb5ac48f6edbe5f355a3e2393811a3edd8
LORDyyyyy <mlord6271@gmail.com>
2023-11-07 18:53:46 +0200

    0-

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+""" """
+import requests
+from sys import argv
+
+
+def number_of_subscribers(subreddit):
+    """ """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    response = requests.get(url, allow_redirects=False)
+
+    if response.status_code == 200:
+        data = response.json()
+        subs = data['data']['subscribers']
+        return (subs)
+    else:
+        return (0)
