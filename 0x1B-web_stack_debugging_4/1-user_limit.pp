374a37569c330ddd409084fde9001e87bfc2868b
LORDyyyyy <mlord6271@gmail.com>
2023-11-16 19:29:43 +0200

    Task 1

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,12 @@
+# change the ulimit of a user
+
+exec { 'modify-holberton-hard':
+  command => 'sed -i "/^holberton hard/s/5/100000/" /etc/security/limits.conf',
+  path    => ['/usr/bin/', '/bin/']
+}
+
+exec { 'modify-holberton-soft':
+  command => 'sed -i "/^holberton soft/s/4/100000/" /etc/security/limits.conf',
+  path    => ['/usr/bin/', '/bin/']
+}
+
